2cb5172a3f744baa6835542b5a511794730445a9
Vietanh12468 <vietanh12468@yahoo.com>
2024-04-04 15:55:54 +0700

    Yes

--- ASM2/Controllers/AccountController.cs
+++ ASM2/Controllers/AccountController.cs
@@ -1,19 +1,23 @@
 ﻿using ASM2.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Reflection;
 using System.Text.Json;
+
 
 namespace ASM2.Controllers
 {
     public class AccountController : Controller
     {
         static List<User>? users = new List<User>();
-        private readonly ILogger<AccountController> _logger;
 
-        public AccountController(ILogger<AccountController> logger)
+        public static List<User>? ReadFileList(String filePath)
         {
-            _logger = logger;
+            // Read a file
+            string readText = System.IO.File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<List<User>>(readText);
         }
+
         [HttpGet]
         public IActionResult Register()
         {
@@ -23,19 +27,40 @@ namespace ASM2.Controllers
         [HttpPost]
         public IActionResult Register(User user)
         {
+            users = ReadFileList("users.json");
             users.Add(user);
             var options = new JsonSerializerOptions { WriteIndented = true };
             string jsonString = JsonSerializer.Serialize(users, options);
             using (StreamWriter writer = new StreamWriter("users.json")) { 
                 writer.Write(jsonString);
             }
-            return Content(jsonString);
+            return RedirectToAction("Search");
         }
 
-        public IActionResult RegistrationSuccess()
+        [HttpGet]
+        public IActionResult Search()
         {
-            return View();
+            users = ReadFileList("users.json");
+            return View(users);
         }
+
+        [HttpPost]
+        public IActionResult Search(string keyword)
+        {
+            List<User> result = new List<User>();
+            users = ReadFileList("users.json");
+            foreach (var user in users)
+            {
+                if (user.fullName != null && user.fullName.ToLower().Contains(keyword.ToLower()))
+                {
+                    result.Add(user);
+                }
+            }
+            return View(result);
+        }
+
+
+
     }
 
 }
